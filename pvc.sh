2513dc8d3a5b45a8a2ca4c95c1e9cbeab68827dc
BOUCHERET <webmasterdu63@gmail.com>
2026-02-06 16:06:29 +0100

    Update pvc.sh

--- pvc.sh
+++ pvc.sh
@@ -3,3 +3,59 @@
 # Version: 0.1
 
 # TODO: Implement CLI commands
+#!/bin/bash
+
+CMD="$1"
+shift
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+case "$CMD" in
+  generate)
+    LENGTH=16
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        --length) LENGTH="$2"; shift ;;
+      esac
+      shift
+    done
+    "$SCRIPT_DIR/../generator/password_generator.sh" --length "$LENGTH" --upper --lower --digits --symbols
+    ;;
+
+  encrypt)
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        --master) MASTER="$2"; shift ;;
+        --input)  INPUT="$2"; shift ;;
+        --output) OUTPUT="$2"; shift ;;
+      esac
+      shift
+    done
+    if [[ -z "$MASTER" || -z "$INPUT" || -z "$OUTPUT" ]]; then
+      echo "Usage: pvc.sh encrypt --master <pwd> --input <file> --output <file>"
+      exit 1
+    fi
+    pwsh "$SCRIPT_DIR/../encryption/encrypt.ps1" -MasterPassword "$MASTER" -InputFile "$INPUT" -OutputFile "$OUTPUT"
+    ;;
+
+  decrypt)
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        --master) MASTER="$2"; shift ;;
+        --input)  INPUT="$2"; shift ;;
+        --output) OUTPUT="$2"; shift ;;
+      esac
+      shift
+    done
+    if [[ -z "$MASTER" || -z "$INPUT" || -z "$OUTPUT" ]]; then
+      echo "Usage: pvc.sh decrypt --master <pwd> --input <file> --output <file>"
+      exit 1
+    fi
+    pwsh "$SCRIPT_DIR/../encryption/decrypt.ps1" -MasterPassword "$MASTER" -InputFile "$INPUT" -OutputFile "$OUTPUT"
+    ;;
+
+  *)
+    echo "Commands: generate | encrypt | decrypt"
+    exit 1
+    ;;
+esac
