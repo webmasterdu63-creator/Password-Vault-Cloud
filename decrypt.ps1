c4cc67ae67c12d057b730729d15dba89c5f40895
BOUCHERET <webmasterdu63@gmail.com>
2026-02-06 16:04:03 +0100

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -2,3 +2,38 @@
 # Version: 0.1
 
 # TODO: Implement AES-256 decryption
+# Password Vault Cloud - AES Decryption
+
+param(
+    [string]$MasterPassword,
+    [string]$InputFile,
+    [string]$OutputFile
+)
+
+. "$PSScriptRoot/crypto_lib.ps1"
+
+$bytes = [System.IO.File]::ReadAllBytes($InputFile)
+
+# Extraction des blocs
+$salt = $bytes[0..15]
+$iv   = $bytes[16..31]
+$data = $bytes[32..($bytes.Length - 1)]
+
+# Dérivation de la clé
+$key = Derive-Key -MasterPassword $MasterPassword -Salt $salt
+
+# Configuration AES
+$aes = [System.Security.Cryptography.Aes]::Create()
+$aes.KeySize = 256
+$aes.BlockSize = 128
+$aes.Mode = "CBC"
+$aes.Padding = "PKCS7"
+$aes.Key = $key
+$aes.IV = $iv
+
+$decryptor = $aes.CreateDecryptor()
+$decrypted = $decryptor.TransformFinalBlock($data, 0, $data.Length)
+
+[System.IO.File]::WriteAllBytes($OutputFile, $decrypted)
+
+Write-Output "Fichier déchiffré : $OutputFile"
